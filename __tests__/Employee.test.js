34cd86d94fee0e5fc0d2284d8fa51dd09febf78c
Satya Selvan <satyaat92@gmail.com>
2022-11-19 20:15:10 +0800

    final error on displaying the team fixed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -6,45 +6,45 @@ test("Can create an new employee.", () => {
 })
 
 test("Testing name.", () => {
-    const name = "James";
+    const name = "Satya";
     const employeeInstance = new Employee(name);
     expect(employeeInstance.name).toBe(name);
 })
 
 test("Testing ID.", () => {
     const id = 2;
-    const employeeInstance = new Employee("James", id);
+    const employeeInstance = new Employee("Satya", id);
     expect(employeeInstance.id).toBe(id);
 })
 
 test("Testing email.", () => {
-    const email = "jamesljenks@gmail.com";
-    const employeeInstance = new Employee("James", 2, email);
+    const email = "satya@gmail.com";
+    const employeeInstance = new Employee("Satya", 2, email);
     expect(employeeInstance.email).toBe(email);
 })
 
 
 
 test("Gets name through getName method.", () => {
-    const testName = "James";
+    const testName = "Satya";
     const employeeInstance = new Employee(testName);
     expect(employeeInstance.getName()).toBe(testName);
 })
 
 test("Can test ID through getID method.", () => {
     const testID = 2;
-    const employeeInstance = new Employee("James", testID);
+    const employeeInstance = new Employee("Satya", testID);
     expect(employeeInstance.getId()).toBe(testID);
 })
 
 test("Can test email through getEmail method.", () => {
     const testEmail = "jamesljenks@gmail.com";
-    const employeeInstance = new Employee("James", 2, testEmail);
+    const employeeInstance = new Employee("Satya", 2, testEmail);
     expect(employeeInstance.getEmail()).toBe(testEmail);
 })
 
 test("Testing role.", () => {
     const returnValue = "Employee";
-    const employeeInstance = new Employee("James", 2, "jamesljenks@gmail.com");
+    const employeeInstance = new Employee("James", 2, "Satya@gmail.com");
     expect(employeeInstance.getRole()).toBe(returnValue);
 })
--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,19 +1,19 @@
 const Engineer = require("../lib/Engineer");
 
 test("Can create a github.", () => {
-    const testGithub = "JamesLJenks";
-    const employeeInstance = new Engineer("James", 2, "jamesljenks@gmail.com", testGithub);
+    const testGithub = "Satya";
+    const employeeInstance = new Engineer("Satya", 2, "Satya@gmail.com", testGithub);
     expect(employeeInstance.github).toBe(testGithub);
 });
 
 test("Testing getGithub will return github.", () => {
     const testGithub = "JamesLJenks";
-    const employeeInstance = new Engineer("James", 2, "jamesljenks@gmail.com", testGithub);
+    const employeeInstance = new Engineer("Satya", 2, "Satya@gmail.com", testGithub);
     expect(employeeInstance.getGithub()).toBe(testGithub);
 });
 
 test("Testing role.", () => {
     const returnValue = "Engineer";
-    const employeeInstance = new Engineer("James", 2, "jamesljenks@gmail.com", "JamesLJenks");
+    const employeeInstance = new Engineer("Satya", 2, "Satya@gmail.com", "Satya");
     expect(employeeInstance.getRole()).toBe(returnValue);
 });
